9068276cceddc31b21e592984ffa84101b22e2d7
ismail duman <67826493+ismail-dmn@users.noreply.github.com>
2026-08-20 19:30:10 +0300

    Add files via upload

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -1,9 +1,4 @@
+"use client"
+import { AuthGate } from "@/components/form/auth-gate"
 import { VehicleForm } from "@/components/form/vehicle-form"
-
-export default function FormPage() {
-  return (
-    <main className="min-h-screen bg-background">
-      <VehicleForm />
-    </main>
-  )
-}
+export default function FormPage(){return <AuthGate>{user=><main className="min-h-screen bg-background"><VehicleForm currentUser={user}/></main>}</AuthGate>}
